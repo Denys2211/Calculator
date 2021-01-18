0583c6c7c2da90309f51e25475d584f25e51f3b8
tsurkanovskyi <tsurkanovskyi@gmail.com>
2021-01-18 18:18:21 +0200

    Сalculations with the priority of multiplication and division

--- Calculator.cs
+++ Calculator.cs
@@ -1,87 +1,128 @@
 ﻿using System;
-using System.Data;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp2
 {
 
-     class Calculator
+    class Calculator
     {
 
         internal void DataEntry(out string input, out string[] symbol)
         {
-            symbol = new [] { "*(", "+(", "-(", "/(", ")*", ")/", ")+", ")-", "-", "+", "/", "*", ")", "(", " (", ") ", };
+            symbol = new[] { "*(", "+(", "-(", "/(", ")*", ")/", ")+", ")-", "-", "+", "/", "*", ")", "(", " (", ") ", };
             Console.Write("Separated MatheXpression: ");
             input = Console.ReadLine();
         }
         internal void Evaluate(string input, out double result)
         {
+                Stack<String> stack = new Stack<String>();
 
-            Stack<string> stack = new Stack<string>();
-
-            string value = "";
-            for (int i = 0; i < input.Length; i++)
-            {
-                string symbol = input.Substring(i, 1); 
-                char chr = symbol.ToCharArray()[0];
-
-                if (!char.IsDigit(chr) && chr != '.' && value != "")
+                string value = "";
+                for (int i = 0; i < input.Length; i++)
                 {
-                    stack.Push(value);
-                    value = "";
-                }
+                    String symbol = input.Substring(i, 1);
+                    char chr = symbol.ToCharArray()[0];
 
-                if (symbol.Equals("("))
-                {
-
-                    string innerExp = "";
-                    i++; //отримати наступний символ
-                    for (; i < input.Length; i++)
+                    if (!char.IsDigit(chr) && chr != '.' && value != "")
                     {
+                        stack.Push(value);
+                        value = "";
+                    }
+                    if (symbol.Equals("("))
+                    {
+                        string innerExp = "";
+                        i++; 
+                        int bracketCount = 0;
+                        for (; i < input.Length; i++)
+                        {
                         symbol = input.Substring(i, 1);
 
-                        if (symbol.Equals(")"))
-                                break;
-                        innerExp += symbol;
+                            if (symbol.Equals("(")) bracketCount++;
+
+                            if (symbol.Equals(")"))
+                            {
+                                if (bracketCount == 0) break;
+                                bracketCount--;
+                            }
+                            innerExp += symbol;
+                        }
+                        Evaluate(innerExp, out result);
+                        stack.Push(result.ToString());
                     }
+                    else if (symbol.Equals("+") ||
+                             symbol.Equals("-") ||
+                             symbol.Equals("*") ||
+                             symbol.Equals("/"))
+                    {
+                        stack.Push(symbol);
+                    }
+                    else if (char.IsDigit(chr) || chr == '.')
+                    {
+                        value += symbol;
+                        if (i == (input.Length - 1))
+                            stack.Push(value);
 
-                    Evaluate(innerExp, out result);//обчислення виразу в дужках
-                    stack.Push(result.ToString());
+                    }
                 }
-                else if (symbol.Equals("+")) stack.Push(symbol);
-                else if (symbol.Equals("-")) stack.Push(symbol);
-                else if (symbol.Equals("*")) stack.Push(symbol);
-                else if (symbol.Equals("/")) stack.Push(symbol);
-               
-                else if (char.IsDigit(chr) || chr == '.')
+                List<String> list = stack.ToList<String>();
+            
+                for (int i = list.Count - 2; i >= 0; i--)
                 {
-                    value += symbol;
-                    if (i == (input.Length - 1))
-                        stack.Push(value);
+                    if (list[i] == "/")
+                    {
+                        list[i] = (Convert.ToDouble(list[i + 1]) / Convert.ToDouble(list[i - 1])).ToString();
+                        list.RemoveAt(i + 1);
+                        list.RemoveAt(i - 1);
+                        i -= 2;
+                    }
                 }
-            }
-            result = 0;
-            while (stack.Count >= 3)
+
+                for (int i = list.Count - 2; i >= 0; i--)
+                {
+                    if (list[i] == "*")
+                    {
+                        list[i] = (Convert.ToDouble(list[i + 1]) * Convert.ToDouble(list[i - 1])).ToString();
+                        list.RemoveAt(i + 1);
+                        list.RemoveAt(i - 1);
+                        i -= 2;
+                    }
+                }
+                for (int i = list.Count - 2; i >= 0; i--)
+                {
+                    if (list[i] == "+")
+                    {
+                        list[i] = (Convert.ToDouble(list[i + 1]) + Convert.ToDouble(list[i - 1])).ToString();
+                        list.RemoveAt(i + 1);
+                        list.RemoveAt(i - 1);
+                        i -= 2;
+                    }
+                }
+                for (int i = list.Count - 2; i >= 0; i--)
+                {
+                    if (list[i] == "-")
+                    {
+                        list[i] = (Convert.ToDouble(list[i + 1]) - Convert.ToDouble(list[i - 1])).ToString();
+                        list.RemoveAt(i + 1);
+                        list.RemoveAt(i - 1);
+                        i -= 2;
+                    }
+                }
+                stack.Clear();
+                for (int i = 0; i < list.Count; i++)
+                {
+                    stack.Push(list[i]);
+                }
+
+                result = Convert.ToDouble(stack.Pop());
+            
+                
+        }
+            internal void OutputDisplay(double result)
             {
-
-                double right = Convert.ToDouble(stack.Pop());
-                string op = stack.Pop();
-                double left = Convert.ToDouble(stack.Pop());
-
-                if (op == "+") result = left + right;
-                else if (op == "-") result = left - right;
-                else if (op == "*") result = left * right;
-                else if (op == "/") result = left / right;
-
-                stack.Push(result.ToString());
+                Console.WriteLine($"Calculation result: {result}");
+                Console.ReadKey();
             }
-            result = Convert.ToDouble(stack.Pop());
-        }
-
-        internal void OutputDisplay(double result)
-        {
-            Console.WriteLine($"Calculation result: {result}");
-            Console.ReadKey();
-        }
-     }
+        
+    }
 }
